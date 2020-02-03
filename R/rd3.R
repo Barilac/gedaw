7365b5516b2d5494da2b65dd0691c45a7f7f7b43
Martin Janovský <janovskym@email.cz>
2020-02-03 14:32:01 +0100

    03 02 2020 Paris

---
+++ R/rd3.R
@@ -0,0 +1,23 @@
+#' Data from rd3 geophysical measurement.
+#'
+#' Data from rd3 geophysical measurement.
+#' Data from rd3 geophysical measurement.
+#' Data from rd3 geophysical measurement.
+#' Data from rd3 geophysical measurement.
+#'
+#' @docType data
+#'
+#' @usage data(rd3)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Moore et al. (2013) Genetics 195:1077-1086
+#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/23979570}{PubMed})
+#'
+#' @source \href{https://phenome.jax.org/projects/Moore1b}{QTL Archive}
+#'
+#' @examples
+#' data(rd3)
+"rd3"
