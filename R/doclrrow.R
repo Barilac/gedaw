b4ec5f36b77d72a53d7db5638261b9a827cfbaa7
Martin Janovský <janovskym@email.cz>
2020-02-04 14:27:43 +0100

    new

--- R/doclrrow.R
+++ R/doclrrow.R
@@ -3,11 +3,13 @@
 #' This function allows you to process clr transformation in rows.
 #' @param base The logarithm base is set to 10
 #' @param a for dim
+#' @param zero zero=T
+#' @param na na=T
 #' @keywords clr
 #' @export
 #' @examples
 #' doclrrow(gpr**2)
-doclrrow = function (a, base=10)
+doclrrow = function (a, zero=T, na=T, base=10)
 
 {
 
